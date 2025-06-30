5d2204022c77052c62e42f7844a3c4eb94a86962
Kemeow815 <kemiao@kmblog.icu>
2025-06-30 18:01:33 +0800

    1

--- src/config.js
+++ src/config.js
@@ -1,43 +1,67 @@
 /**
- * 这是我个人的博客配置文件
- * 请根据文档复制 config.sample.js 文件并重命名为 config.js
- * 并修改为对应配置项
+ * 博客配置文件，没有说明的就是必填项
+ * @type {Object}
+ * @property {String} title         博客标题
+ * @property {String} subtitle      博客副标题
+ * @property {String} description   博客描述
+ * @property {String} keywords      博客关键字
+ * @property {String} logo          博客图标 180x180
+ * @property {String} favicon16     博客图标 16x16
+ * @property {String} favicon32     博客图标 32x32
+ * @property {String} createdTime   博客创建时间
+ * @property {String} icp           ICP 备案号
+ * @property {String} icpLink       ICP 备案链接
+ * @property {String} beian         [选填] 公安备案号 '京公网安备 00000000000000号'
+ * @property {String} beianLink     [选填] 公安备案链接 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=00000000000000'
+ * @property {String} username      GitHub 用户名
+ * @property {String} repository    GitHub 仓库名
+ * @property {String} friendsRepo   友链仓库名
+ * @property {String} author        博客作者
+ * @property {String} email         博客作者邮箱
+ * @property {String} link          个人链接
+ * @property {String} github        GitHub 链接
+ * @property {String} name          友链名称
+ * @property {String} url           友链链接
+ * @property {String} avatar        友链头像
+ * @property {String} desc          友链描述
+ * @property {String} pageSize      文章列表每页显示文章数
+ * @property {String} defaultCover  文章默认封面图
  */
 
 export default {
   // 博客基础配置
-  title: 'wallleap',
-  subtitle: 'ルー・ワン',
-  description: 'Luwang\'s blog',
-  keywords: 'Luwang, blog, wallleap',
+  title: '喵落阁',
+  subtitle: '克喵的博客',
+  description: '克喵在博客里分享自己的生活经历、资源分享和踩坑教程等等。',
+  keywords: '克喵, blog, github',
   logo: './logo.svg',
   favicon16: './favicon.ico',
   favicon32: './favicon.ico',
-  createdTime: '2019-08-01',
-  icp: '赣ICP备20000895号-1',
-  icpLink: '//beian.miit.gov.cn/',
+  createdTime: '2025-06-30',
+  icp: '',
+  icpLink: '',
   beian: '',
   beianLink: '',
   // 关于页二维码
   alipay_qr: '/images/alipayQr.jpg',
   // GitHub Issues 配置
-  username: 'wallleap',
-  repository: 'myblogs',
-  // friendsRepo: 'friends', // 留空则默认使用上方仓库获取
+  username: 'Kemeow815',
+  repository: 'kemiao-blog',
+  friendsRepo: 'kemiao-blog-friends', // 留空则默认使用上方仓库获取
   // 您的信息
-  author: 'Luwang',
-  email: 'luwang@oicode.cn',
-  link: '//luwang.info',
-  github: '//github.com/wallleap',
+  author: '克喵爱吃卤面',
+  email: 'me@kemiaosw.top',
+  link: 'https://www.kemiao.online',
+  github: 'https://github.com/Kemeow815',
   // 友链信息
-  name: 'wallleap',
-  url: '//myblog.wallleap.cn',
-  avatar: '//gravatar.wallleap.cn/avatar/be1ccdcf025a92b98a92e331e1b3662a?size=256',
-  desc: 'Luwang\'s blog',
+  name: '喵落阁',
+  url: 'https://myblog.kemeow.top/',
+  avatar: '作者或博客头像链接',
+  desc: '愿你看清一切真相后，依旧热爱你的家人和朋友。',
+
   // 文章相关
   pageSize: 12,
   defaultCover: '/images/cover.jpg',
-  // 图片加载失败时显示，请将图片下载至你自己的存储空间，并使用自己的 CDN 引入
   errorImg: '/images/error.gif',
   // Twikoo 评论配置
   twikoo: {
